9b984c856b0792b7ef602877606e5abf4445792f
loic khodarkovsky <lkhodarkovsky@gmail.com>
2023-06-01 15:50:03 +0200

    minDate

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -8,7 +8,8 @@ export default class extends Controller {
     dateFormat: 'Y-m-d H:i',
     altInput: true,
     altFormat: "F j, Y",
-    dateFormat: "Y-m-d"
+    dateFormat: "Y-m-d",
+    minDate: "today"
     }
   )};
 }
